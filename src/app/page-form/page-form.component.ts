cb220f0612a6859f9af7a4fd44d8fa7d27f54b5c
gabriel quesnot <gaqu1994@gmail.com>
2023-05-12 09:44:46 +0200

    add path validation

--- src/app/page-form/page-form.component.ts
+++ src/app/page-form/page-form.component.ts
@@ -1,7 +1,16 @@
 import {Component, Inject, Optional} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Page} from "../services/page";
+
+
+
+function containsSlash(control: AbstractControl) {
+  if (!control.value.includes('/')) {
+    return { noSlash: true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-page-form',
@@ -19,11 +28,24 @@ export class PageFormComponent {
     this.pageForm = this.fb.group({
       name: [data ? data.name : '', Validators.required],
       content: [data ? data.content : '', Validators.required],
-      path: [data ? data.path : '', Validators.required]
+      path: [data ? data.path : '', [Validators.required, containsSlash]]
     });
   }
 
+  getPathErrorMessage() {
+    if (this.pageForm.controls['path'].hasError('required')) {
+      return 'You must enter a value';
+    }
+    if (this.pageForm.controls['path'].hasError('noSlash')) {
+      return 'Path must contain a slash';
+    }
+    return '';
+  }
+
   save() {
+    if (this.pageForm.invalid) {
+      return;
+    }
     this.dialogRef.close(this.pageForm.value);
   }
 }
